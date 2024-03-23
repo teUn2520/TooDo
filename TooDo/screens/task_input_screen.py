9c25c13242629e4bbae306e16fa39f9365047f72
Dmitriy Ivanko <tmwsls12@gmail.com>
2024-03-23 15:49:40 +0300

    Fix

--- TooDo/screens/task_input_screen.py
+++
@@ -1,8 +0,0 @@
-from hammett.core.constants import EMPTY_KEYBOARD
-
-from screens.schedule_screen import ScheduleScreen
-
-
-class TaskInputScreen(ScheduleScreen):
-    description = "Теперь поставь задачу, просто напиши в чат и я добавлю ее в твое расписание."
-    keyboard = EMPTY_KEYBOARD
