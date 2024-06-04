a89093317aafb1a026b2ca2fbd7a90db95f920dc
Nikita Startsev <teun25201@gmail.com>
2024-06-04 13:39:00 +0300

    Edit local IP and add models in admin panel

--- backend/apps/DatabaseForm/admin.py
+++ backend/apps/DatabaseForm/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from backend.apps.DatabaseForm.models import TelegramUser, TelegramTask
+
 # Register your models here.
+admin.site.register(TelegramUser)
+admin.site.register(TelegramTask)
